f2863ff768120d2d63825c747eb1e2d4936ebbfb
Juan Manuel Pérez <jmperez.85@gmail.com>
2020-01-08 18:53:25 -0300

    Add multiprocessing to speed up the process

--- bin/find_upstream_tweets.py
+++ bin/find_upstream_tweets.py
@@ -3,18 +3,37 @@ from mongoengine import connect
 from tweepyrate import create_apps
 import fire
 import tweepy
+from multiprocessing import Pool
 from tqdm import tqdm
 from hate_collector.models import Tweet
 
+def fetch_and_save(*args):
+    # TODO: Fix this. I don't know why I should do this way
+    app, tweet_id = args[0]
+    try:
+        status = app.get_status(tweet_id, tweet_mode="extended")
+        tweet = Tweet(**status._json)
+        tweet.save()
+        return (tweet_id, tweet)
+    except tweepy.TweepError as e:
+        return (tweet_id, e)
+
 
 def fetch_tweets(apps, tweet_ids):
-    for app, tweet_id in tqdm(zip(cycle(apps), tweet_ids), total=len(tweet_ids)):
-        try:
-            status = app.get_status(tweet_id, tweet_mode="extended")
-            tweet = Tweet(**status._json)
-            tweet.save()
-        except tweepy.TweepError as e:
-            print(e)
+    with Pool(len(apps)) as p:
+        pbar = tqdm(total=len(tweet_ids))
+
+        def update_pbar(*args, **kwargs):
+            pbar.update()
+
+        iterator = p.imap(
+            fetch_and_save,
+            zip(cycle(apps), tweet_ids)
+        )
+
+        # This hack is just to make tqdm work.
+        list(tqdm(iterator, total=len(tweet_ids)))
+
 
 def find_upstream_tweets(database):
     """
