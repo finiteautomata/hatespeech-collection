d01fb39df14dba4cf632ff2017c31628d879d2fc
Juan Manuel Pérez <jmperez.85@gmail.com>
2020-01-20 23:41:53 -0300

    Improve script for searching removed tweets.

--- bin/search_removed_tweets.py
+++ bin/search_removed_tweets.py
@@ -1,52 +1,39 @@
 import time
 from itertools import cycle
 from multiprocessing import Pool
-from tqdm import tqdm
 from mongoengine import connect, ValidationError, NotUniqueError
-from tweepyrate import create_apps
+from tweepyrate import create_apps, get_tweets
 import fire
 import tweepy
 from hate_collector.models import Tweet, APIError
 
-def fetch_and_save(*args):
-    # TODO: Fix this. I don't know why I should do this way
-    app, tweet_id = args[0]
+
+def save_error(tweet_id, response):
     try:
-        status = app.get_status(tweet_id, tweet_mode="extended")
-        return (tweet_id, status)
-    except tweepy.TweepError as e:
-        return (tweet_id, e)
+        error = APIError(
+            message=str(response),
+            api_code=response.api_code,
+            tweet_id=tweet_id
+        )
+
+        error.save()
+
+    except (NotUniqueError, ValidationError) as e:
+        pass
+    except ValidationError as e:
+        print(e)
+
 
 
 def fetch_errors(apps, tweet_ids):
-    with Pool(len(apps)) as p:
-        iterator = p.imap(
-            fetch_and_save,
-            zip(cycle(apps), tweet_ids)
-        )
+    new_tweets, new_errors = get_tweets(
+        apps,
+        tweet_ids,
+        lambda _1, _2: None,
+        save_error,
+    )
 
-        # This hack is just to make tqdm work.
-        ret = list(tqdm(iterator, total=len(tweet_ids)))
-
-        print("Saving just errors")
-        new_errors = 0
-        for tweet_id, response in ret:
-            if type(response) is tweepy.TweepError:
-                try:
-                    error = APIError(
-                        message=str(response),
-                        api_code=response.api_code,
-                        tweet_id=tweet_id
-                    )
-
-                    error.save()
-
-                    new_errors += 1
-                except (NotUniqueError, ValidationError) as e:
-                    continue
-        print(f"There are {new_errors} new errors")
-
-
+    print(f"There are {new_tweets} new tweets and {new_errors} new errors")
 
 def search_removed_tweets(database):
     """
@@ -54,13 +41,15 @@ def search_removed_tweets(database):
     """
     apps = create_apps("config/my_apps.json")
 
-    connect(database)
+    client = connect(database)
+    db = client[database]
 
-    tweets = Tweet.objects(
-        query=None,
+    tweets = db.tweet.find(
+        { "query": None },
+        { "_id": 1 },
     )
 
-    tweet_ids = [tw.id for tw in tweets]
+    tweet_ids = [tw["_id"] for tw in tweets]
 
     print(f"\nThere are {tweets.count()} to look for in our database")
     fetch_errors(apps, tweet_ids)
