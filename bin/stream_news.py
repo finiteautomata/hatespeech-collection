5ea56c50b860937de92604d5699b848ae82b31cb
Juan Manuel Pérez <jmperez.85@gmail.com>
2020-02-05 15:17:24 -0300

    Add script to stream tweets about news.

--- bin/stream_news.py
+++ bin/stream_news.py
@@ -5,18 +5,37 @@ import random
 import tweepy
 from hate_collector import TweetListener
 
-default_queries = ["racista", "misógino", "machista", "machirulo"]
+default_queries = [
+    "@infobae",
+    "@LANACION",
+    "@clarincom",
+    "@cronica",
+    "@perfilcom",
+    #Diarios Españoles
+    "@elmundoes",
+    #Uruguayos
+    "@elpaisuy",
+    # Chilenos
+    "@latercera", # Diario chileno
+ ]
 
 
-def stream(database, queries=default_queries):
+def stream_news(database, queries=default_queries):
     """
-    Look for replies using any of the words (queries)
+    Look for tweets mentioning (or from) any of these newspapers
+
+    Arguments:
+    ----------
+
+    database: Name of the mongodatabase
+
+    queries: list
+        List of terms (or @usernames) to look for
     """
     apps = create_apps("config/my_apps.json")
     random.shuffle(apps)
 
     connect(database)
-
 
     print(f"Looking for {queries}")
 
@@ -24,10 +43,10 @@ def stream(database, queries=default_queries):
         app = apps[i % len(apps)]
         print(f"Creating listener for {word}")
 
-        myStreamListener = TweetListener(word, only_replies=True)
+        myStreamListener = TweetListener(word)
         myStream = tweepy.Stream(auth = app.auth, listener=myStreamListener)
         myStream.filter(track=[word], is_async=True, languages=["es"])
 
 
 if __name__ == '__main__':
-    fire.Fire(stream)
+    fire.Fire(stream_news)
