c8699b0651e1cc161fe4e3bc0dcce2b1f5b897c7
Juan Manuel Pérez <jmperez.85@gmail.com>
2020-04-16 00:00:34 -0300

    Fix issue with big articles

--- bin/generate_instances.py
+++ bin/generate_instances.py
@@ -1,40 +1,58 @@
+import time
 import fire
 from multiprocessing import Pool
 from mongoengine import connect
 from tqdm.auto import tqdm
 import newspaper as ns
-from hatespeech_models import Article
+from hatespeech_models import Article, Tweet
 from hate_collector.article import download_article
 import tweepy
 
 
+def download_article_and_replies(tweet):
+    art = download_article(tweet)
+    # Now ret should be a dict with "body" and "title" keys
+    replies = Tweet.objects(in_reply_to_status_id=tweet["_id"]).as_pymongo()
+
+    return (art, replies)
 
 def download_articles(tweets, num_workers):
     with Pool(num_workers) as p:
         total = len(tweets)
-        articles = list(tqdm(p.imap(download_article, tweets), total=total))
+        articles = list(tqdm(p.imap(download_article_and_replies, tweets), total=total))
 
-        for tw, art in zip(tweets, articles):
+        for tw, (art, replies) in zip(tweets, articles):
             if art:
                 tw["article"] = art
+                tw["replies"] = replies
 
 
-def generate_instances(database, num_workers=4):
+def generate_instances(database, num_workers=4, clean_before=True, rebuild=True):
     """
     Generate instances for annotation
     """
 
     client = connect(database)
     db = client[database]
-    print(f"Looking for possibly hateful news and their comments")
+    print("Creating articles and their comments")
+
+    if clean_before:
+        print(f"Cleaning {Article.objects.count()} objects")
+        Article.objects.delete()
 
     screen_names = [t[1:].lower() for t in db.tweet.distinct('query') if t is not None]
     print(f"Screen names: {' - '.join(screen_names)}")
+
+    """
+    This is another approach: getting everything from the database at the
+    very beginning. However, I'm getting stuck with the maximum db size of mongo
+    begin = time.time()
     tweets = db.tweet.aggregate([
         {
             "$match": {
                 "user_name": {"$in": screen_names },
-                "possibly_hateful_comments": True
+                "in_reply_to_status_id": None,
+                "retweeted_status": None,
             }
         },
         {
@@ -45,12 +63,18 @@ def generate_instances(database, num_workers=4):
                 "as": "replies"
             }
         },
+        {
+            "$unwind": "$replies"
+        }
     ])
-
+    """
+    tweets = Tweet.objects(
+        user_name__in=screen_names,
+        in_reply_to_status_id=None,
+        retweeted_status=None
+    ).as_pymongo()
     tweets = list(tweets)
-    len_replies = len([1 for tweet in tweets for reply in tweet["replies"]])
-    print(f"There are {len(tweets)/1000:.2f}K tweets and {len_replies/1000:.2f}K replies\n\n")
-
+    print(f"There are {len(tweets)/1000:.2f}K news\n\n")
     print("Searching for articles...")
     download_articles(tweets, num_workers)
     # Now "article" should be in each tweet
